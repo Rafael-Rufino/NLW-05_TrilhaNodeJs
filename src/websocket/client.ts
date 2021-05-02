bc9014ab324d76a5dfeef5a68a6fea09d0efe738
rafaelrufino <rafaelrufino.52@gmail.com>
2021-05-02 04:37:02 -0300

    Aula 05 - concluida com sucesso, sistema de chat terminado

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -21,9 +21,10 @@ io.on("connect", (socket) => {
     let user_id = null;
 
 
-    const userExists = await usersService.create(email);
+    const userExists = await usersService.findByEmail(email);
+
     if (!userExists) {
-      const user = await usersService.findByEmail(email);
+      const user = await usersService.create(email);
 
       await connectionsService.create({
         socket_id,
@@ -52,7 +53,35 @@ io.on("connect", (socket) => {
       user_id,
 
     });
-
     //Salver a conexao com o socket_id, user_id
+    const allMessages = await messagesService.listByUser(user_id);
+
+    socket.emit("client_list_all_messages", allMessages);
+
+    const allUsers = await connectionsService.findAllWithoutAdmin();
+    io.emit("admin_list_all_users", allUsers);
+
+
+
+  });
+
+  socket.on("client_send_to_admin", async (params) => {
+    const { text, socket_admin_id } = params;
+
+    const socket_id = socket.id;
+
+    const { user_id } = await connectionsService.findBySocketID(socket_id);
+
+    const message = await messagesService.create({
+      text,
+      user_id,
+    });
+
+    io.to(socket_admin_id).emit("admin_receive_message", {
+      message,
+      socket_id,
+    });
+
+
   });
 });
